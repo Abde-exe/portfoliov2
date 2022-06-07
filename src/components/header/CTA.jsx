399ceffae3e1bbc4a882246d6dcbefc13bb8659a
Abde-exe <abdessamadbou91@gmail.com>
2022-06-07 09:33:01 +0200

    no index on google

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -3,7 +3,7 @@ import CV from '../../assets/pdf/CvAbdessamadWebMobile.pdf';
 function CTA() {
   return (
     <div className="cta">
-      <a href={CV} rel="noreferrer" className="btn" target="_blank">
+      <a href={CV} download className="btn">
         Télécharger CV
       </a>
       <a href="#contact" className="btn btn-primary">
